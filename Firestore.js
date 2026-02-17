a57def17c98c88d0836464930d3e3a44548942f9
kinxintruder-droid <kinxintruder@gmail.com>
2026-02-17 14:35:37 +0200

    Delete Firestore.js

--- Firestore.js
+++
@@ -1,7 +0,0 @@
-match /withdrawals/{id} {
-  allow create: if request.auth != null;
-  allow read: if request.auth != null;
-  allow update: if exists(
-    /databases/$(database)/documents/admins/$(request.auth.uid)
-  );
-}
